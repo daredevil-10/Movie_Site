1d2920920e8f5534d62c56db261503b14aed987a
= <vipuly41@gmail.com>
2025-08-05 13:19:18 +0530

    Movie app completed

--- MovieSite/src/App.jsx
+++ MovieSite/src/App.jsx
@@ -4,7 +4,7 @@ import Spinner from "./components/spinner";
 import MovieCard from "./components/MovieCard";
 
 import { useDebounce} from "react-use";
-import { updateSearchCount } from "./appwrite";
+import { getTrendingMovies, updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -23,6 +23,7 @@ function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const[isLoading,setIsLoading]=useState(false);
+  const[trendingMovies,setTrendingMovies] = useState([]);
   const [debouncedSearchTerm , setDebouncedSearchTerm] = useState('');
 
   useDebounce(()=>
@@ -47,7 +48,10 @@ function App() {
         return;
       }
       setMovieList(data.results || []);
-      updateSearchCount();
+      if(query && data.results.length > 0)
+      {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (err) {
       console.error(`Error fetching movies:${err}`);
       setErrorMessage("Error fetching movies. Please try again later.");
@@ -55,9 +59,27 @@ function App() {
       setIsLoading(false);
     }
   };
+
+  const loadTrendingMovies = async()=>{
+    try
+    {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    }
+    catch(err)
+    {
+      console.error(`Error fetching trending movies: ${err}`);
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
+
+  useEffect(()=>{
+    loadTrendingMovies()
+  },[]);
 
   return (
     <main>
@@ -73,8 +95,21 @@ function App() {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
+        {trendingMovies.length>0 && (
+          <section className="trending">
+            <h2>Trending</h2>
+            <ul>
+              {trendingMovies.map((movie,index)=>(
+                <li key={movie.$id}>
+                  <p>{index+1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
         <section className="all-movies">
-          <h2 className="mt-[40px]">All Movies</h2>
+          <h2 >All Movies</h2>
 
           {isLoading ? (
             <Spinner/>
--- MovieSite/src/appwrite.js
+++ MovieSite/src/appwrite.js
@@ -1,7 +1,53 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASEID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTIONID;
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECTID;
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
 
-export const updateSearchCount = async ()=>{
-    console.log(PROJECT_ID,COLLECTION_ID,DATABASE_ID)
+const client = new Client()
+ .setEndpoint(ENDPOINT)
+ .setProject(PROJECT_ID)
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm,movie)=>{
+    try
+    {
+        const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID,[Query.equal('searchTerm',searchTerm)])
+
+        if(result.documents.length>0)
+        {
+            const doc = result.documents[0];
+
+            await database.updateDocument(DATABASE_ID,COLLECTION_ID,doc.$id,{
+                count: doc.count+1
+            })
+
+        } else {
+            await database.createDocument(DATABASE_ID,COLLECTION_ID,ID.unique(),{
+                searchTerm,
+                count: 1, 
+                movie_id: movie.id,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+            })
+        }
+
+    } catch(err)
+    {
+        console.error(err);
+    }
+}
+
+export const getTrendingMovies = async ()=>{
+    try
+    {
+        const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID,[Query.limit(5),Query.orderDesc('count')])
+
+        return result.documents;
+
+    }catch(err){
+        console.error(err);
+        
+    }
 }
